4769a76a1349427e0f4454080c4539cb8f882c0e
Hunter <hunterstingel@gmail.com>
2022-08-27 14:42:30 -0400

    0827Progress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-import sys, time, requests, json
+import sys, time, requests, json, schedule
 
 sys.path = ['/home/hwstingel/SeniorProject/Airport-StatTrac', '/usr/lib/python39.zip', '/usr/lib/python3.9', '/usr/lib/python3.9/lib-dynload', '/home/hwstingel/.local/lib/python3.9/site-packages', '/usr/local/lib/python3.9/dist-packages', '/usr/lib/python3/dist-packages', '/usr/lib/python3.9/dist-packages']
 
@@ -12,10 +12,11 @@ airport = 'KCLT'
 startTimeList = ['00','06','12','18']
 toTimeList = ['06','12','18','00']
 
-while(True):
-    today = datetime.now().strftime('%Y-%m-%d')
-    yesterday = (datetime.now() - timedelta(1)).strftime('%Y-%m-%d')
+def logFlights():
+    today = (datetime.now() - timedelta(3)).strftime('%Y-%m-%d')
+    yesterday = (datetime.now() - timedelta(4)).strftime('%Y-%m-%d')
     urlList = []
+    
     for x in range(4):
         if x == 3:
             urlList.append("https://aerodatabox.p.rapidapi.com/flights/airports/icao/{}/{}T{}:00/{}T{}:00".format(airport,yesterday,startTimeList[x],today,toTimeList[x]))
@@ -33,9 +34,54 @@ while(True):
     print(len(fullListDep))
     print(len(fullListArr))
     
-    while True:
-        continue
+    cancDep = 0
+    cancArr = 0
     
+    aircraftListDep = []
+    aircraftListArr = []
+    airlineListDep = {}
+    airlineListArr = {}
+    
+    for each in fullListDep:
+        if "Canceled" in each['status']:
+            cancArr += 1
+        if "aircraft" in each.keys():
+            if "model" in each["aircraft"].keys():
+                if each["aircraft"]["model"] not in aircraftListDep:
+                    aircraftListDep.append(each["aircraft"]["model"])
+        if "airline" in each.keys():
+            if each["airline"]["name"] not in airlineListDep.keys():
+                airlineListDep[each["airline"]["name"]] = 1
+            else:
+                airlineListDep[each["airline"]["name"]] += 1
+        
+    for each in fullListArr:
+        if "Canceled" in each['status']:
+            cancDep += 1      
+        if "aircraft" in each.keys():
+            if "model" in each["aircraft"].keys():
+                if each["aircraft"]["model"] not in aircraftListArr:
+                    aircraftListArr.append(each["aircraft"]["model"])
+        if "airline" in each.keys():
+            if each["airline"]["name"] not in airlineListArr.keys():
+                airlineListArr[each["airline"]["name"]] = 1
+            else:
+                airlineListArr[each["airline"]["name"]] += 1
+    
+    
+    print(aircraftListDep)
+    print(aircraftListArr)
+    
+    print(airlineListDep)
+    print(airlineListArr)
+    
+    print(cancDep)
+    print(cancArr)
+    
+schedule.every().day.at("8:00").do(logFlights)
+
+while True:
+    schedule.run_pending()
     time.sleep(60)
         
         
